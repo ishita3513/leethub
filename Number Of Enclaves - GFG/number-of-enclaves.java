759fac210bf944475cc56e125152ceb2d4517358
Ishita Kumari <85451704+ishita3513@users.noreply.github.com>
2023-05-17 18:55:09 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.java
+++ Number Of Enclaves - GFG/number-of-enclaves.java
@@ -37,22 +37,40 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    void dfs(int[][]grid, int i,int j,int[][] vis){
-        int n=grid.length,m=grid[0].length;
-        if(i<0 || j<0 || i>=n || j>=m || vis[i][j]==1 || grid[i][j]==0)return;
-        vis[i][j]=1;
-        dfs(grid,i+1,j,vis);
-        dfs(grid,i-1,j,vis);
-        dfs(grid,i,j+1,vis);
-        dfs(grid,i,j-1,vis);
-    }
-    int numberOfEnclaves(int[][] grid) {
+    int numberOfEnclaves(int grid[][]){
         int n=grid.length,m=grid[0].length;
         int[][] vis=new int[n][m];
+        Queue<List<Integer>>q=new LinkedList<>();
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 || j==0 ||i==n-1 || j==m-1 ){
-                    dfs(grid,i,j,vis);
+                if((i==0 || j==0 ||i==n-1 || j==m-1) && grid[i][j]==1 ){
+                    // dfs(grid,i,j,vis);
+                    vis[i][j]=1;
+                    List<Integer>list=new ArrayList<>();
+                    list.add(i);
+                    list.add(j);
+                    q.add(list);
+                }
+            }
+        }
+        
+        int[] delR={-1,0,1,0};
+        int[] delC={0,-1,0,1};
+        while(!q.isEmpty()){
+            int size=q.size();
+            int r=q.peek().get(0);
+            int c=q.peek().get(1);
+            q.poll();
+            for(int i=0;i<4;i++){
+                int nr=delR[i]+r;
+                int nc=delC[i]+c;
+                if(nr>=0 && nc>=0 && nr<n && nc<m && vis[nr][nc]==0 && grid[nr][nc]==1){
+                    vis[nr][nc]=1;
+                    List<Integer>list=new ArrayList<>();
+                    list.add(nr);
+                    list.add(nc);
+                    q.add(list);
                 }
             }
         }
@@ -60,7 +78,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 // System.out.print(vis[i][j]+" ");
-                if(vis[i][j]==0 && grid[i][j]==1)cnt++;
+                if(vis[i][j]==0 && grid[i][j]==1)cnt++;;
             }
             // System.out.println();
         }
