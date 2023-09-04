0a340183261c90426fda36318105214a5e47d9f4
Ishita Kumari <85451704+ishita3513@users.noreply.github.com>
2023-09-04 12:06:36 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.java
+++ Replace O's with X's - GFG/replace-os-with-xs.java
@@ -39,34 +39,33 @@ class GFG{
 
 //User function Template for Java
 
-
-class Solution {
-    static void dfs(char[][]grid, int i,int j,int[][] vis){
-        int n=grid.length,m=grid[0].length;
-        if(i<0 || j<0 || i>=n || j>=m || vis[i][j]==1 || grid[i][j]=='X')return;
+class Solution{
+    static void dfs(int[][] vis,char[][] a,int i,int j){
+        if(i<0 || j<0 || i>=vis.length || j>=vis[0].length || vis[i][j]==1 || a[i][j]=='X')return;
         vis[i][j]=1;
-        dfs(grid,i+1,j,vis);
-        dfs(grid,i-1,j,vis);
-        dfs(grid,i,j+1,vis);
-        dfs(grid,i,j-1,vis);
+        dfs(vis,a,i+1,j);
+        dfs(vis,a,i,j+1);
+        dfs(vis,a,i-1,j);
+        dfs(vis,a,i,j-1);
     }
-    static char[][] fill(int n, int m, char grid[][]){
+    static char[][] fill(int n, int m, char a[][]){
+        //if somebody is not attached to boundry and visted then mark then as X
+        //apply dfs on top,bottom,left, right boundry element
         int[][] vis=new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 || j==0 ||i==n-1 || j==m-1 ){
-                    dfs(grid,i,j,vis);
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    dfs(vis,a,i,j);
                 }
             }
         }
-        int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                // System.out.print(vis[i][j]+" ");
-                if(vis[i][j]==0)grid[i][j]='X';
+                if(vis[i][j]==0 && a[i][j]=='O'){
+                    a[i][j]='X';
+                }
             }
-            // System.out.println();
         }
-        return grid;
+        return a;
     }
 }
