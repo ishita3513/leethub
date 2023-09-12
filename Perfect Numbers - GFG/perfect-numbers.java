a99711aed327708579d4f752c4b86c97248928a4
Ishita Kumari <85451704+ishita3513@users.noreply.github.com>
2023-09-12 10:11:51 +0530

    Added solution - LeetHub

---
+++ Perfect Numbers - GFG/perfect-numbers.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.isPerfectNumber(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    static int isPerfectNumber(long N) {
+        long ans=(long)Math.sqrt(N);
+        if(N==1)return 0;
+        long cnt=1;
+        for(long i=2;i<=ans;i++){
+            if(N%i==0){
+                cnt+=i;
+                cnt+=(N/i);
+            }
+        }
+        if(cnt==N)return 1;
+        return 0;
+    }
+};
