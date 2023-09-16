27588862687c0b630b8f4bf1e28e476235c8fc91
Ishita Kumari <85451704+ishita3513@users.noreply.github.com>
2023-09-16 12:56:26 +0530

    Added solution - LeetHub

---
+++ Count number of hops - GFG/count-number-of-hops.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG implements Runnable {
+    public void run() {
+        try {
+            BufferedReader in;
+            PrintWriter out;
+            in = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(System.out);
+            int t = Integer.parseInt(in.readLine());
+
+            while (t-- > 0) {
+                int n = Integer.parseInt(in.readLine().trim());
+
+                Solution g = new Solution();
+                out.println(g.countWays(n));
+            }
+            out.close();
+        } catch (Exception e) {
+            ;
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        new Thread(null, new GFG(), "whatever", 1 << 26).start();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    static long solve(int n,long[] dp){
+        if(n<0)return 0;
+        if(n==0)return 1;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=((solve(n-1,dp)+solve(n-2,dp))%1000000007+solve(n-3,dp))%1000000007;
+    }
+    //Function to count the number of ways in which frog can reach the top.
+    static long countWays(int n)
+    {
+        long[] dp=new long[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+    
+}
+
